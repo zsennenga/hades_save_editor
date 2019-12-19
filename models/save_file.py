ed54e761f9323a084b7a8f7cfd9138c255fd61f0
zennenga <astrozees@gmail.com>
2019-12-18 22:18:06 -0800

    Fix up checksum logic and schema structure

--- models/save_file.py
+++ models/save_file.py
@@ -1,9 +1,7 @@
-import zlib
 from io import BytesIO
 
 from luabins import decode_luabins
 
-from constant import FILE_SIGNATURE
 from schemas.sav_14 import sav14_schema
 
 
@@ -11,7 +9,9 @@ class HadesSaveFile:
     def __init__(self, raw_bytes: bytes):
         self.parsed_schema = sav14_schema.parse(raw_bytes)
 
-        self.lua_state = decode_luabins(BytesIO(bytes(self.parsed_schema.lua_state)))
+        self.lua_state = decode_luabins(
+            BytesIO(bytes(self.parsed_schema.save_data.value.lua_state))
+        )
 
     @classmethod
     def from_file(cls, path):
@@ -19,8 +19,7 @@ class HadesSaveFile:
             return HadesSaveFile(f.read())
 
     def to_file(self, path):
-        save_data = sav14_schema.save_data.build(self.parsed_schema.save_data)
-        with open(path, 'wb') as f:
-            f.write(FILE_SIGNATURE)
-            f.write(zlib.adler32(save_data).to_bytes(4, "little"))
-            f.write(save_data)
+        sav14_schema.build_file(
+            {'save_data': self.parsed_schema.save_data},
+            filename=path,
+        )
--- schemas/sav_14.py
+++ schemas/sav_14.py
@@ -1,29 +1,42 @@
+import zlib
+
 from construct import *
 
 from constant import FILE_SIGNATURE
 
-sav14_schema = Padded(
-    3145728,
-    Struct(
-        "signature" / Const(FILE_SIGNATURE),
-        "checksum" / Int32ul,
-        "save_data" / Struct(
-            "version" / Int32ul,
-            "location" / PascalString(Int32ul, "utf8"),
-            "runs" / Int32ul,
-            "active_meta_points" / Int32ul,
-            "active_shrine_points" / Int32ul,
-            "easy_mode" / Byte,
-            "hard_mode" / Byte,
-            "key_count" / Int32ul,
-            "keys" / Array(
-                this.key_count,
-                PascalString(Int32ul, "utf8")
-            ),
-            "current_map_name" / PascalString(Int32ul, "utf8"),
-            "start_next_map" / PascalString(Int32ul, "utf8"),
-            "lua_state_length" / Int32ul,
-            "lua_state" / Array(this.lua_state_length, Byte)
+sav14_schema = Struct(
+    "signature" / Const(FILE_SIGNATURE),
+    "checksum_offset" / Tell,
+    "checksum" / Padding(4),
+    "save_data_offset" / Tell,
+    "save_data" / RawCopy(
+        Padded(
+            3145728 - this.save_data_offset,
+            Struct(
+                "version" / Int32ul,
+                "location" / PascalString(Int32ul, "utf8"),
+                "runs" / Int32ul,
+                "active_meta_points" / Int32ul,
+                "active_shrine_points" / Int32ul,
+                "easy_mode" / Byte,
+                "hard_mode" / Byte,
+                "keys" / PrefixedArray(
+                    Int32ul,
+                    PascalString(Int32ul, "utf8")
+                ),
+                "current_map_name" / PascalString(Int32ul, "utf8"),
+                "start_next_map" / PascalString(Int32ul, "utf8"),
+                "lua_state" / PrefixedArray(Int32ul, Byte)
+            )
+        )
+    ),
+    "checksum" / Pointer(
+        this.checksum_offset,
+        Checksum(
+            Int32ul,
+            lambda data: zlib.adler32(data, 1),
+            this.save_data.data
         )
     )
+
 )
