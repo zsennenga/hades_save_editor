325a93fb60ce3febd6f18d07997839fd26a94d8c
zachary <astrozees@gmail.com>
2020-02-28 16:00:43 -0800

    Update

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import os
 import sys
 
 from PyQt5 import QtWidgets, uic
-from PyQt5.QtWidgets import QFileDialog, QPushButton, QLineEdit, QMessageBox, QDialog, QLabel
+from PyQt5.QtWidgets import QFileDialog, QPushButton, QLineEdit, QMessageBox, QDialog, QLabel, QCheckBox, QWidget
 
 from models.save_file import HadesSaveFile
 
+mainWin = None
 
 def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
@@ -18,6 +19,13 @@ def resource_path(relative_path):
     return os.path.join(base_path, relative_path)
 
 
+def except_hook(cls, exception, traceback):
+
+    mainWin.error_widget.setVisible(1)
+    mainWin.error_label.setText("{}: {}".format(exception.__class__.__name__, str(exception)))
+    sys.__excepthook__(cls, exception, traceback)
+
+
 class App(QDialog):
     def __init__(self, application):
         super().__init__()
@@ -28,6 +36,13 @@ class App(QDialog):
         self.dirty = False
 
         uic.loadUi(resource_path('pluto.ui'), self)  # Load the .ui file
+        self.error_widget = self.findChild(QWidget, "errorWidget")
+        self.error_label = self.findChild(QLabel, "errorValue")
+
+        self.ui_state = self.findChild(QLabel, "state")
+
+        self.error_widget.setVisible(0)
+
         self.load_button = self.findChild(QPushButton, "load")
         self.load_button.clicked.connect(self.open_file_name_dialog)
 
@@ -36,6 +51,9 @@ class App(QDialog):
 
         self.exit_button = self.findChild(QPushButton, "exit")
         self.exit_button.clicked.connect(self.safe_quit)
+
+        self.interaction_reset = self.findChild(QPushButton, "interactionReset")
+        self.interaction_reset.clicked.connect(self.reset_gift_record)
 
         self.path_label = self.findChild(QLabel, "pathValue")
         self.version_label = self.findChild(QLabel, "versionValue")
@@ -49,6 +67,8 @@ class App(QDialog):
         self.ambrosia_field = self.findChild(QLineEdit, "ambrosiaEdit")
         self.keys_field = self.findChild(QLineEdit, "chthonicKeyEdit")
         self.titan_blood_field = self.findChild(QLineEdit, "titanBloodEdit")
+
+        self.hell_mode = self.findChild(QCheckBox, "hellModeCheckbox")
 
         self.show()  # Show the GUI
 
@@ -65,6 +85,8 @@ class App(QDialog):
         self.file_path = fileName
         self.save_file = HadesSaveFile.from_file(self.file_path)
 
+        self.error_widget.setVisible(0)
+
         self.path_label.setText(fileName)
         self.version_label.setText(str(self.save_file.version))
         self.run_label.setText(str(self.save_file.runs))
@@ -77,8 +99,10 @@ class App(QDialog):
         self.ambrosia_field.setText(str(self.save_file.lua_state.ambrosia))
         self.keys_field.setText(str(self.save_file.lua_state.chthonic_key))
         self.titan_blood_field.setText(str(self.save_file.lua_state.titan_blood))
+        self.hell_mode.setChecked(bool(self.save_file.lua_state.hell_mode))
 
         self.dirty = True
+        self.ui_state.setText("Loaded!")
 
     def write_file(self):
         self.save_file.lua_state.darkness = float(self.darkness_field.text())
@@ -88,9 +112,21 @@ class App(QDialog):
         self.save_file.lua_state.ambrosia = float(self.ambrosia_field.text())
         self.save_file.lua_state.chthonic_key = float(self.keys_field.text())
         self.save_file.lua_state.titan_blood = float(self.titan_blood_field.text())
+        self.save_file.lua_state.hell_mode = bool(self.hell_mode.isChecked())
+        self.save_file.hell_mode_enabled = bool(self.hell_mode.isChecked())
 
         self.save_file.to_file(self.file_path)
         self.dirty = False
+        self.ui_state.setText("Saved!")
+
+    def reset_gift_record(self):
+        self.save_file.lua_state.gift_record = {}
+        self.save_file.lua_state.npc_interactions = {}
+        self.save_file.lua_state.trigger_record = {}
+        self.save_file.lua_state.activation_record = {}
+        self.save_file.lua_state.use_record = {}
+        self.save_file.lua_state.text_lines = {}
+        self.ui_state.setText("Reset NPC gifting status")
 
     def safe_quit(self):
         if self.dirty:
@@ -105,4 +141,9 @@ class App(QDialog):
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     mainWin = App(app)
+
+    import sys
+
+    sys.excepthook = except_hook
+
     sys.exit(app.exec_())
