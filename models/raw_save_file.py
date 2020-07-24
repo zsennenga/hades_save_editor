ec7af81b8e34f8ae5c8d40eec2454939a0c33cb4
Ronald Libago <me@ronaldlibago.dev>
2020-07-23 19:17:26 -0700

    Update raw_save_file.py with v16
    Includes a minor refactor from if-else statements to dictionaries for
    version selection.

--- models/raw_save_file.py
+++ models/raw_save_file.py
@@ -1,9 +1,10 @@
 from typing import Dict, Any
 
 from bin_utils import rpad_bytes
-from constant import SAVE_DATA_V14_LENGTH, SAVE_DATA_V15_LENGTH
+from constant import SAVE_DATA_V14_LENGTH, SAVE_DATA_V15_LENGTH, SAVE_DATA_V16_LENGTH
 from schemas.sav_14 import sav14_schema, sav14_save_data_schema
 from schemas.sav_15 import sav15_schema, sav15_save_data_schema
+from schemas.sav_16 import sav16_schema, sav16_save_data_schema
 from schemas.version_id import version_identifier_schema
 
 
@@ -20,13 +21,17 @@ class RawSaveFile:
     @classmethod
     def from_file(cls, path: str) -> 'RawSaveFile':
         with open(path, 'rb') as f:
+            parser_dict = {
+                14: sav14_schema,
+                15: sav15_schema,
+                16: sav16_schema
+            }
+
             input_bytes = f.read()
             version = version_identifier_schema.parse(input_bytes).version
 
-            if version == 14:
-                parsed_schema = sav14_schema.parse(input_bytes)
-            elif version == 15:
-                parsed_schema = sav15_schema.parse(input_bytes)
+            if version in parser_dict:
+                parsed_schema = parser_dict[version].parse(input_bytes)
             else:
                 raise Exception(f"Unsupported version {version}")
 
@@ -36,29 +41,33 @@ class RawSaveFile:
             )
 
     def to_file(self, path: str) -> None:
-        if self.version == 14:
-            sav14_schema.build_file(
+        build_dictionary = {
+            14: {
+                'schema': sav14_schema,
+                'save_data_schema': sav14_save_data_schema,
+                'length': SAVE_DATA_V14_LENGTH
+            },
+            15: {
+                'schema': sav15_schema,
+                'save_data_schema': sav15_save_data_schema,
+                'length': SAVE_DATA_V15_LENGTH
+            },
+            16: {
+                'schema': sav16_schema,
+                'save_data_schema': sav16_save_data_schema,
+                'length': SAVE_DATA_V16_LENGTH
+            }
+        }
+
+        if self.version in build_dictionary:
+            build_dictionary[self.version]['schema'].build_file(
                 {
                     'save_data': {
                         'data': rpad_bytes(
-                            sav14_save_data_schema.build(
+                            build_dictionary[self.version]['save_data_schema'].build(
                                 self.save_data
                             ),
-                            SAVE_DATA_V14_LENGTH
-                        )
-                    }
-                },
-                filename=path,
-            )
-        elif self.version == 15:
-            sav15_schema.build_file(
-                {
-                    'save_data': {
-                        'data': rpad_bytes(
-                            sav15_save_data_schema.build(
-                                self.save_data
-                            ),
-                            SAVE_DATA_V15_LENGTH
+                            build_dictionary[self.version]['length']
                         )
                     }
                 },
