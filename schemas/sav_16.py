d958edbafbe029da19a7cdcbb0dde964983de127
Ronald Libago <me@ronaldlibago.dev>
2020-07-23 18:50:16 -0700

    Add sav_16.py
    This v16 schema includes an Int64ul timestamp

---
+++ schemas/sav_16.py
@@ -0,0 +1,44 @@
+import zlib
+
+from construct import *
+
+from constant import FILE_SIGNATURE, SAVE_DATA_V16_LENGTH
+
+sav16_save_data_schema = Struct(
+    "version" / Int32ul,
+    "timestamp" / Int64ul,
+    "location" / PascalString(Int32ul, "utf8"),
+    "runs" / Int32ul,
+    "active_meta_points" / Int32ul,
+    "active_shrine_points" / Int32ul,
+    "god_mode_enabled" / Byte,
+    "hell_mode_enabled" / Byte,
+    "lua_keys" / PrefixedArray(
+        Int32ul,
+        PascalString(Int32ul, "utf8")
+    ),
+    "current_map_name" / PascalString(Int32ul, "utf8"),
+    "start_next_map" / PascalString(Int32ul, "utf8"),
+    "lua_state" / PrefixedArray(Int32ul, Byte)
+)
+
+sav16_schema = Struct(
+    "signature" / Const(FILE_SIGNATURE),
+    "checksum_offset" / Tell,
+    "checksum" / Padding(4),
+    "save_data" / RawCopy(
+        Padded(
+            SAVE_DATA_V16_LENGTH,
+            sav16_save_data_schema
+        )
+    ),
+    "checksum" / Pointer(
+        this.checksum_offset,
+        Checksum(
+            Int32ul,
+            lambda data: zlib.adler32(data, 1),
+            this.save_data.data
+        )
+    )
+
+)
