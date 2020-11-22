74d8be2fa137f3a379a47e9b0861c9e05b07102c
Michel van de Wetering <michel.van.de.wetering@gmail.com>
2020-11-22 21:35:08 +0100

    Add exporting of runs as csv

---
+++ gamedata.py
@@ -0,0 +1,44 @@
+''' Contains Hades gamedata '''
+
+# Based on data in Weaponsets.lua
+HeroMeleeWeapons = {
+    "SwordWeapon": "Stygian Blade",
+    "SpearWeapon": "Eternal Spear",
+    "ShieldWeapon": "Shield of Chaos",
+    "BowWeapon": "Heart-Seeking Bow",
+    "FistWeapon": "Twin Fists of Malphon",
+    "GunWeapon": "Adamant Rail",
+}
+
+
+# Based on data from TraitData.lua
+AspectTraits = {
+    "SwordCriticalParryTrait": "Nemesis",
+    "SwordConsecrationTrait": "Arthur",
+    "ShieldRushBonusProjectileTrait": "Chaos",
+    "ShieldLoadAmmoTrait": "Beowulf",
+    # "ShieldBounceEmpowerTrait": "",
+    "ShieldTwoShieldTrait": "Zeus",
+    "SpearSpinTravel": "Guan Yu",
+    "GunGrenadeSelfEmpowerTrait": "Eris",
+    "FistVacuumTrait": "Talos",
+    "FistBaseUpgradeTrait": "Zagreus",
+    "FistWeaveTrait": "Demeter",
+    "FistDetonateTrait": "Gilgamesh",
+    "SwordBaseUpgradeTrait": "Zagreus",
+    "BowBaseUpgradeTrait": "Zagreus",
+    "SpearBaseUpgradeTrait": "Zagreus",
+    "ShieldBaseUpgradeTrait": "Zagreus",
+    "GunBaseUpgradeTrait": "Zagreus",
+    "DislodgeAmmoTrait": "Poseidon",
+    # "SwordAmmoWaveTrait": "",
+    "GunManualReloadTrait": "Hestia",
+    "GunLoadedGrenadeTrait": "Lucifer",
+    "BowMarkHomingTrait": "Chiron",
+    "BowLoadAmmoTrait": "Hera",
+    # "BowStoredChargeTrait": "",
+    "BowBondTrait": "Rama",
+    # "BowBeamTrait": "",
+    "SpearWeaveTrait": "Hades",
+    "SpearTeleportTrait": "Achilles",
+}
